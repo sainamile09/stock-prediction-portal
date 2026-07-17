e6046ddaa375364724a65cec98218092c65a4f5f
saikrishna <saikrishna.bhuvi@gmail.com>
2026-07-17 13:34:35 +0530

    added axiosInstance

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics,permissions
 from rest_framework.permissions import AllowAny
 from accounts.Serializers import UserSerializers
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView 
+from rest_framework.response import Response
 
 
 # Create your views here.
@@ -11,3 +14,12 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializers
     permission_classes = [AllowAny]
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        response={
+            'status':'Request was Permitted'
+        }
+        return Response(response)
--- backend-drf/apis/urls.py
+++ backend-drf/apis/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('protected-view/',views.ProtectedView.as_view(),name='protected-view')
 ]
